c133bdc3739e60c5650cca47e0297c93eec4518c
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2022-04-25 17:56:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,9 @@ st.text("I-Este App realiza backtest em ações do indice Bovespa,Brasil Amplo e
 st.text("II-O Dados são obtidos pela API do yahoo finance;")
 st.text("III-Todos as operações são do tipo Swing Trade com parametros abertos;")
 st.text("IV- Os backtest são realizados para um periodo dos últimos cinco anos")
-names = ['Lucas Campos','Leandro Giron','Daniel Solano','Rafael Quintanilha']
-passwords = ['lucassomatoria7@gmail.com','leandro.giron@gmail.com','contato@quantinvestimentos.com.br','rafael@quantbrasil.com.br']
+names = ['Lucas Campos','Leandro Giron','Daniel Solano','Rafael Quintanilha','Diogo Pereira']
+passwords = ['lucassomatoria7@gmail.com','leandro.giron@gmail.com','contato@quantinvestimentos.com.br','rafael@quantbrasil.com.br'
+            'Diogo.sapia@gmail.com']
 usuario = st.text_input("Insira seu nome de usuário : ")
 senha = st.text_input("Insira sua senha : ")
 if senha:
